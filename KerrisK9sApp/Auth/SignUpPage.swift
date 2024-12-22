29e2a1c5593769951ccd06d3644d739f81db8f28
scolicodes <mscoli44@gmail.com>
2024-12-21 22:49:36 -0500

    [Clean] Changes file structure for auth-related files

---
+++ KerrisK9sApp/Auth/SignUpPage.swift
@@ -0,0 +1,72 @@
+//
+//  SignUpPage.swift
+//  KerrisK9sApp
+//
+//  Created by Michael Scoli on 12/21/24.
+//
+
+import SwiftUI
+
+struct SignUpPage: View {
+    @State private var email: String = ""
+    @State private var password: String = ""
+    @State private var confirmPassword: String = ""
+
+    var body: some View {
+        VStack {
+            Spacer()
+            Text("Sign Up")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundColor(Color.lightPink)
+                .padding(.bottom, 20)
+            TextField("Email", text: $email)
+                .padding()
+                .background(Color(.systemGray6))
+                .cornerRadius(8)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 8)
+                        .stroke(Color.lightPink, lineWidth: 1)
+                    )
+                .padding(.horizontal, 16)
+            SecureField("Password", text: $password)
+                .padding()
+                .background(Color(.systemGray6))
+                .cornerRadius(8)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 8)
+                        .stroke(Color.lightPink, lineWidth: 1)
+                    )
+                .padding(.horizontal, 16)
+                .padding(.top, 10)
+            SecureField("Confirm Password", text: $confirmPassword)
+                .padding()
+                .background(Color(.systemGray6))
+                .cornerRadius(8)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 8)
+                        .stroke(Color.lightPink, lineWidth: 1)
+                    )
+                .padding(.horizontal, 16)
+                .padding(.top, 10)
+            Button(action: {
+                // Handle sign-up
+            }) {
+                Text("Sign Up")
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(Color.lightPink)
+                    .cornerRadius(8)
+                    .padding(.horizontal, 16)
+            }
+            .padding(.top, 20)
+            Spacer()
+        }
+        .padding(.vertical, 20)
+        .background(Color.white)
+        .ignoresSafeArea()
+    }
+}
+
---
+++ KerrisK9sApp/Extensions/ColorExtensions.swift
@@ -0,0 +1,13 @@
+//
+//  ColorExtensions.swift
+//  KerrisK9sApp
+//
+//  Created by Michael Scoli on 12/21/24.
+//
+
+import SwiftUI
+
+extension Color {
+    static let lightPink = Color(UIColor(red: 204/255, green: 51/255, blue: 153/255, alpha: 1.0)) // Hex: #cc3399
+}
+
